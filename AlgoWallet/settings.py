0cedb0fd034abba0d2e611d71012e246a1d4c090
divyansh <knr.guptadivyansh@gmail.com>
2022-03-28 17:50:48 +0530

    added ip address

--- AlgoWallet/settings.py
+++ AlgoWallet/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-w_q^n(ro921fd46+!oh8o%xt&zqya!72jo)bae5lo@%#mj1v=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.56.24']
   
 
 #Sending mail

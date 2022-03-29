00def76b2d3eebab5e2768a24fd2b1b570c9ed96
divyansh <knr.guptadivyansh@gmail.com>
2022-03-29 11:28:34 +0530

    debug =false

--- AlgoWallet/settings.py
+++ AlgoWallet/settings.py
@@ -33,7 +33,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'django-insecure-w_q^n(ro921fd46+!oh8o%xt&zqya!72jo)bae5lo@%#mj1v=o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['139.59.56.24','*']
   

7afc0c01dfeacf33d0a29562af60486e88ebf8fe
divyansh <knr.guptadivyansh@gmail.com>
2022-03-29 15:39:42 +0530

    ter

--- AlgoWallet/settings.py
+++ AlgoWallet/settings.py
@@ -150,9 +150,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-if DEBUG:
-    STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')

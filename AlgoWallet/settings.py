3821c8ab590d85f026d7bdbf3da3c4109b841cb2
divyansh <knr.guptadivyansh@gmail.com>
2022-03-29 11:16:25 +0530

    added host

--- AlgoWallet/settings.py
+++ AlgoWallet/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-w_q^n(ro921fd46+!oh8o%xt&zqya!72jo)bae5lo@%#mj1v=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['139.59.56.24']
+ALLOWED_HOSTS = ['139.59.56.24','*']
   
 
 #Sending mail

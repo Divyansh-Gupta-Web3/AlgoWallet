70b527d8c537c531a239b50653fd580ea4c55cfa
divyansh <knr.guptadivyansh@gmail.com>
2022-03-29 11:30:34 +0530

    remove sqldb

--- AlgoWallet/settings.py
+++ AlgoWallet/settings.py
@@ -90,25 +90,17 @@ WSGI_APPLICATION = 'AlgoWallet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wallet',
+        'USER':'Algopy',
+        'PASSWORD':'wallet123',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
-else:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'wallet',
-            'USER':'Algopy',
-            'PASSWORD':'wallet123',
-            'HOST':'localhost',
-            'PORT':''
-        }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
